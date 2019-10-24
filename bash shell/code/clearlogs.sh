ed270e83b4db14edeb9024a844778454d5b8298f
OddExtension5 <sushil.kisu@gmail.com>
2019-10-24 20:47:20 +0530

    added clearlogs.sh

---
+++ bash shell/code/clearlogs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script copies /var/log contents and clears current contents of the file
+# Usage: ./clearlogs
+
+cp /var/log/messages /var/log/messages.old
+cat /dev/null > /var/log/messages
+
+echo log file copied and cleaned up
+
+exit 0
