728346ccf0e7ab7abc8754186fc63bf0aa788875
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2019-10-26 16:03:00 +0530

    Create heredoc1.sh

---
+++ bash shell/code/heredoc1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Script that shows how here dpcuments can be used
+# as an alternative to the echo command
+
+cat <<End-of-message
+-------------------------------------------------
+This is line 1 of the message
+This is line 2 of the message
+This is the last line of the message
+-------------------------------------------------
+End-of-message
