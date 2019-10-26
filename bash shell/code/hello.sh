5365871a1491b86c05d794160fc128fbc816b949
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2019-10-26 12:12:26 +0530

    Create hello.sh

---
+++ bash shell/code/hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# this is a demo script that greets the world
+# Usage: ./hello
+# If it is not executable then use : chmod +x hello.sh  ( to make it executable )
+
+clear
+echo hello world
+
+exit 0
