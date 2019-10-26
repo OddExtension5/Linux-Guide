04b1f3095ddc9c7252cdf19084c3d2419d291868
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2019-10-26 15:31:02 +0530

    Create arg_or_read.sh

---
+++ bash shell/code/arg_or_read.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Script that shows how to make sure that user input is provided
+
+if [ -z $1 ]
+then
+            echo provide filenames
+            read $FILENAMES
+else
+            FILENAMES="$@"
+fi
+
+echo the following filenames have been provided: $FILENAMES
