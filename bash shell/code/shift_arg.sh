5a31584136ef1ae30f02fed33ca2c254dea2fd8b
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2019-10-26 15:42:38 +0530

    Update shift_arg.sh

--- bash shell/code/shift_arg.sh
+++ bash shell/code/shift_arg.sh
@@ -4,7 +4,9 @@
 
 echo "The arguments provided are $*"
 echo "The value of \$1 is $1"
+echo "The entire string is $*"
 shift
 echo "The new value of \$1 is $1"
+echo "The entire string now is S*"
 
 exit
