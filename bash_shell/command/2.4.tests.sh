73fff03d32ce677c7ce160c59309692b39762f30
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2019-10-31 23:24:33 +0530

    Create 2.4.tests.sh

---
+++ bash_shell/command/2.4.tests.sh
@@ -0,0 +1,20 @@
+[ 1 = 0 ]
+echo $?
+[ 1 = 1 ]
+echo $?
+[ 0 == 0 ]
+echo $?
+[ 1 == 1 ]
+echo $?
+A=1
+[ $A = 1 ]
+echo $?
+[ $A = 2 ]
+echo $?
+builtin [
+which [
+builtin [ ]
+which test
+man test
+man [
+test 1 == 1
